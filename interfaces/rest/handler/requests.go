3a36154863068151e8df824b05c5e26e4f15333b
Franco Valencia <franco.avalencia@gmail.com>
2024-09-22 09:16:25 -0600

    Implemented BlobResponse for rest handler

--- interfaces/rest/handler/requests.go
+++ interfaces/rest/handler/requests.go
@@ -8,6 +8,7 @@ import (
 	"github.com/vanclief/ez"
 )
 
+// TODO: Rename this to JSONResponse
 func (h *BaseHandler) StandardRequest(c echo.Context, op string, request requests.Request, body requests.Body) error {
 	request.SetBody(body)
 
@@ -19,6 +20,7 @@ func (h *BaseHandler) StandardRequest(c echo.Context, op string, request request
 	return c.JSON(http.StatusOK, response)
 }
 
+// TODO: Rename this to BindedJSONResponse
 func (h *BaseHandler) BindedRequest(c echo.Context, op string, request requests.Request, body requests.Body) error {
 	if managedError := c.Bind(body); managedError != nil {
 		return h.ManageError(c, op, request, ez.New(op, ez.EINVALID, managedError.Error(), managedError))
@@ -34,6 +36,7 @@ func (h *BaseHandler) BindedRequest(c echo.Context, op string, request requests.
 	return c.JSON(http.StatusOK, response)
 }
 
+// TODO: Rename this to BindedXMLResponse
 func (h *BaseHandler) BindedRequestXMLResponse(c echo.Context, op string, request requests.Request, body requests.Body) error {
 	if managedError := c.Bind(body); managedError != nil {
 		return h.ManageError(c, op, request, ez.New(op, ez.EINVALID, managedError.Error(), managedError))
@@ -47,4 +50,22 @@ func (h *BaseHandler) BindedRequestXMLResponse(c echo.Context, op string, reques
 	}
 
 	return c.XMLPretty(http.StatusOK, response, "  ")
+}
+
+func (h *BaseHandler) BlobResponse(c echo.Context, op string, request requests.Request, contentType string, body requests.Body) error {
+	request.SetBody(body)
+
+	response, managedError := h.App.HandleRequest(request)
+	if managedError != nil {
+		return h.ManageError(c, op, request, managedError)
+	}
+
+	bytes, ok := response.([]byte)
+	if !ok {
+		return h.ManageError(c, op, request, ez.New(op, ez.EINTERNAL, "HandleRequest response is not a byte slice", nil))
+	}
+
+	c.Response().Header().Set("Content-Type", contentType)
+
+	return c.Blob(http.StatusOK, "application/pdf", bytes)
 }
